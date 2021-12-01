bd1dc8db57b093e1bc2aa0567a8fb085330f8d3a
Victor Bondarenko <nirasimha@gmail.com>
2021-12-01 01:39:13 +0000

    server routes change

--- server.js
+++ server.js
@@ -42,24 +42,15 @@ app.use(express.static("public"));
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
-// const usersRoutes = require("./routes/users");
-const quizzesRoutes = require("./routes/quizzes");
 const root = require("./routes/index");
+const makersRoutes = require("./routes/makers");
+const takersRoutes = require("./routes/takers");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
-// app.use("/users", usersRoutes(db));
-app.use("/quizzes", quizzesRoutes(db));
 app.use("/", root(db));
-// Note: mount other resources here, using the same pattern above
-
-// Home page
-// Warning: avoid creating more routes in this file!
-// Separate them into separate routes files (see above).
-
-// app.get("/", (req, res) => {
-//  res.render('index');
-// });
+app.use("/makers", makersRoutes(db));
+app.use("/takers", takersRoutes(db));
 
 
 app.listen(PORT, () => {
