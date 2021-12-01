f7eb1cd01bbe0c0486eff90b73eaa0b565b10204
Victor Bondarenko <nirasimha@gmail.com>
2021-12-01 16:03:21 +0000

    show result

--- routes/takers.js
+++ routes/takers.js
@@ -5,18 +5,20 @@ module.exports = (db) => {
 
   router.get("/:id/", (req, res) => {
     const id = req.params.id;
-    db.query(
-      `SELECT quizzes.id as quiz_id, quizzes.title, users.username as name, questions.question as question,
-      (SELECT answers.value
+    db.query(`
+      SELECT quizzes.id as quiz_id, quizzes.title, users.username as name, questions.question as question,
+        (SELECT answers.value
         FROM answers
         JOIN questions on question_id = questions.id
         JOIN quizzes ON quizzes.id = questions.id
-        WHERE questions.quiz_id = ${id} AND answers.value = 'true') as answer1,
-      (SELECT answers.value
-        FROM answers
-        JOIN questions on question_id = questions.id
-        JOIN quizzes ON quizzes.id = questions.id
-        WHERE questions.quiz_id = ${id} AND answers.value IS NULL) as answer2
+        WHERE questions.quiz_id = ${id}
+        AND answers.value = 'true') as answer1,
+          (SELECT answers.value
+          FROM answers
+          JOIN questions on question_id = questions.id
+          JOIN quizzes ON quizzes.id = questions.id
+          WHERE questions.quiz_id = ${id}
+          AND answers.value IS NULL) as answer2
       FROM quizzes
       JOIN users on quizzes.owner_id = users.id
       JOIN questions ON quizzes.id = quiz_id
@@ -26,9 +28,32 @@ module.exports = (db) => {
       .then((data) => {
         const templateVars = { quiz: { ...data.rows[0] } };
         res
-          .render("take-quiz", templateVars);
+          .render("quiz", templateVars);
       })
       .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
+  router.get("/:id/result/", (req, res) => {
+    const id = req.params.id;
+    db.query(`
+       SELECT previous_attempts.quiz_id as quiz_id, quizzes.title as title, users.username as name, count(questions.is_correct = TRUE) as score
+       FROM previous_attempts
+       JOIN quizzes ON quizzes.id = previous_attempts.quiz_id
+       JOIN users ON users.id = user_id
+       JOIN questions ON quizzes.id = quiz_id
+       WHERE previous_attempts.id = ${id}
+       GROUP BY score;
+       `)
+      .then((data) => {
+        const templateVars = { quiz: { ...data.rows[0] } };
+        res
+          .render("quiz_result", templateVars);
+      })
+      .catch((err) => {
         res
           .status(500)
           .json({ error: err.message });
