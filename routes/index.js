09300cb4066a582130ce8bdea51d235be9f892ad
Victor Bondarenko <nirasimha@gmail.com>
2021-12-01 01:31:37 +0000

    query LIMIT changed

--- routes/index.js
+++ routes/index.js
@@ -6,15 +6,14 @@ const router  = express.Router();
 module.exports = (db) => {
 
   router.get('/', (req, res) => {
-    req.session.user_id = req.params.user_id;
     db.query(`
     SELECT title, description
     FROM quizzes
     WHERE public = true
-    LIMIT 3;
+    LIMIT 10;
     `)
       .then(data => {
-        const templateVar = {quizzes: data.rows, user_id: req.params.user_id};
+        const templateVar = {quizzes: data.rows};
         res.render('../views/index', templateVar);
       });
   });
