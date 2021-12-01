550183f13c290fc91e959560864166c6e8696051
Victor Bondarenko <nirasimha@gmail.com>
2021-12-01 17:11:48 +0000

    quiz taker added with result view

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,9 @@ module.exports = (db) => {
 
   router.get('/', (req, res) => {
     db.query(`
-    SELECT title, description
+    SELECT title, description, user.username as name
     FROM quizzes
+    JOIN users ON users.id = owner_id
     WHERE public = true
     LIMIT 10;
     `)
