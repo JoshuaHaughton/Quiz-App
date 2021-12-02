f959045ea935545dbc2c0d1a6c6a16b8bbcd37ad
Victor Bondarenko <nirasimha@gmail.com>
2021-12-02 17:47:16 +0000

    minor changes to query  questions from quiz

--- routes/takers.js
+++ routes/takers.js
@@ -6,8 +6,7 @@ module.exports = (db) => {
   router.get("/:id/", (req, res) => {
     const id = req.params.id;
     db.query(`
-      SELECT quizzes.id as quiz_id, quizzes.title as title, users.username as name, questions.question as question
-
+      SELECT quizzes.id as quiz_id, quizzes.title, users.username as name, questions.question as question
       FROM quizzes
       JOIN users on quizzes.owner_id = users.id
       JOIN questions ON quizzes.id = quiz_id
@@ -24,37 +23,7 @@ module.exports = (db) => {
       });
   });
 
-  router.get("/:id/result/", (req, res) => {
-    const id = req.params.id;
-    db.query(`
-       SELECT previous_attempts.quiz_id as quiz_id, quizzes.title as title, users.username as name, count(questions.is_correct = TRUE) as score
-       FROM previous_attempts
-       JOIN quizzes ON quizzes.id = previous_attempts.quiz_id
-       JOIN users ON users.id = user_id
-       JOIN questions ON quizzes.id = quiz_id
-       WHERE previous_attempts.id = ${id}
-       GROUP BY score;
-       `)
-      .then((data) => {
-        const templateVars = { quiz: { ...data.rows[0] } };
-        res
-          .render('../views/quiz_result', templateVars);
-      })
-      .catch((err) => {
-        res.status(500).json({ error: err.message });
-      });
-  });
 
-  router.post("/:id/", (req, res) => {
-    const id = req.params.id;
-    db.query("SELECT 1")
-      .then(() => {
-        res.redirect(`/takers/${id}/results`);
-      })
-      .catch((err) => {
-        res.status(500).json({ error: err.message });
-      });
-  });
 
   return router;
 };
