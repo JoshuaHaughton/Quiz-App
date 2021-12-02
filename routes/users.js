56d4958583f54fe78d4ac8972d2afb05fd57640a
Joshua Haughton <itsjoshuahaughton@gmail.com>
2021-12-02 17:55:05 +0000

    test

--- routes/users.js
+++ routes/users.js
@@ -4,13 +4,9 @@ const bodyParser = require("body-parser");
 
 // Home page Routes
 module.exports = (db) => {
+
   // Render the maker new quiz page
   maker.get("/:id/new", (req, res) => {
-    db.query(`
-    INSERT INTO quizzes (owner_id, title, description) values (11, 'Tripterocalyx micranthus (Torr.) Hook.', 'Laniaurius atrococcineus');
-    SELECT id
-    FROM quizzes
-    WHERE title = $1;`, [title])
     res.render("new_quiz");
   });
 
