30cc4ca2c387dfbb6327107a73cbd76205b06c3b
Victor Bondarenko <nirasimha@gmail.com>
2021-12-03 02:14:35 +0000

    show the the questions in table structure

--- routes/takers.js
+++ routes/takers.js
@@ -6,7 +6,7 @@ module.exports = (db) => {
   router.get("/:id/", (req, res) => {
     const id = req.params.id;
     db.query(`
-      SELECT questions.question as questions
+      SELECT questions.question as questions, answer, question
       FROM quizzes
       JOIN users on quizzes.owner_id = users.id
       JOIN questions ON quizzes.id = quiz_id
@@ -15,7 +15,6 @@ module.exports = (db) => {
       `)
       .then((data) => {
         const templateVars = { questions: data.rows };
-        console.log(templateVars);
         res
           .render('../views/quiz', templateVars);
       })
@@ -27,16 +26,17 @@ module.exports = (db) => {
   router.get("/:id/result/", (req, res) => {
     const id = req.params.id;
     db.query(`
-       SELECT previous_attempts.quiz_id as quiz_id, quizzes.title as title, users.username as name, count(questions.is_correct = TRUE) as score
+       SELECT previous_attempts.quiz_id as quiz_id, quizzes.title as title, users.username as name,  is_correct, answer, question,
        FROM previous_attempts
        JOIN quizzes ON quizzes.id = previous_attempts.quiz_id
        JOIN users ON users.id = user_id
+       JOIN answers ON questions.id = question_id
        JOIN questions ON quizzes.id = quiz_id
        WHERE previous_attempts.id = ${id}
        GROUP BY score;
-       `)
+       `, [id])
       .then((data) => {
-        const templateVars = { quiz: { ...data.rows[0] } };
+        const templateVars = { quiz: data.rows};
         res
           .render('../views/quiz_result', templateVars);
       })
@@ -59,3 +59,27 @@ module.exports = (db) => {
 
   return router;
 };
+
+
+
+/*
+router.post("/:quiz_id/attempts", (req, res) => {
+
+    db.getCorrectAnswer(req.params.quiz_id)
+      .then(results => {
+        console.log(results);
+        let score = 0;
+        const numOfQuestions = results.length;
+
+        for (const question of results) {
+          if (req.body[question.question_id] == question.answer_id) {
+            score++;
+          }
+        }
+        //add attempt to the attempts table
+        const attempt = {
+          quizId: req.params.quiz_id,
+          score
+        };
+
+*/
